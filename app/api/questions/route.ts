54d7422fa969baa25f6fbba998752dfbe068c774
Adil Ashurov <ashurovadil96@gmail.com>
2024-05-09 16:17:39 +0400

    work

--- app/api/questions/route.ts
+++ app/api/questions/route.ts
@@ -2,10 +2,21 @@ import { connectMongoDB } from "@/lib/mongodb";
 import Question from "@/models/question";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url)
+    const type = searchParams.get('type') as string; 
+   
+  
+  
+    
     await connectMongoDB();
     try {
-        const data = await Question.find({});
+        let data;
+        if(type) {
+             data = await ( Question.find({"type": type})).limit(30)
+        }
+       
+        
         return NextResponse.json({ data }, { status: 200 });
     } catch (err) {
         console.error(err); // Log the error for debugging purposes
